cb76b352862382c05bf784b50df98122933fd805
Justin Caratzas <bigjust@lambdaphil.es>
2017-08-27 01:01:38 -0400

    move stream setup to join signal, add tweet subcommand

--- helga_twitter.py
+++ helga_twitter.py
@@ -1,14 +1,17 @@
+import smokesignal
 import tweepy
 
 from twisted.internet import reactor
 
-from helga import settings
+from helga import settings, log
 from helga.plugins import Command
 
 CONSUMER_KEY = getattr(settings, 'TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = getattr(settings, 'TWITTER_CONSUMER_SECRET')
 ACCESS_TOKEN = getattr(settings, 'TWITTER_ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = getattr(settings, 'TWITTER_ACCESS_SECRET')
+
+logger = log.getLogger(__name__)
 
 
 class HelgaStreamListener(tweepy.StreamListener):
@@ -33,18 +36,34 @@ class TwitterPlugin(Command):
 
     def run(self, client, channel, nick, message, cmd, args):
 
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        api = tweepy.API(auth)
+        if args and args[0] in ['tweet', 'follow']:
+            auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+            api = tweepy.API(auth)
 
-        myStream = tweepy.Stream(
-            auth = api.auth,
-            listener = HelgaStreamListener(
-                client=client,
-                channel=channel
-            )
+            subcmd = args[0]
+
+            if subcmd == 'tweet':
+                status = api.update_status(' '.join(args[1:]))
+                return 'https://twitter.com/{}/status/{}'.format(
+                    status.author.screen_name,
+                    status.id,
+                )
+
+
+@smokesignal.on('join')
+def init_twitter_stream(client, channel):
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    myStream = tweepy.Stream(
+        auth = api.auth,
+        listener = HelgaStreamListener(
+            client=client,
+            channel=channel
         )
+    )
 
-        myStream.userstream(async=True)
-
-        return 'Initialized Twitter listener...'
+    myStream.userstream(async=True)
